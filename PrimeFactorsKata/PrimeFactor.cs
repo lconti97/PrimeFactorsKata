2dfbb87568556d8125bb1d05109cc7172047b200
Lucas Conti <lconti@qualtrax.com>
2017-05-18 13:34:44 -0400

    Created solution and pass simple tests

---
+++ PrimeFactorsKata/PrimeFactor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrimeFactorsKata
+{
+    public class PrimeFactor
+    {
+        public Int32[] Generate(Int32 num)
+        {
+            if (num == 1)
+                return null;
+            else
+                return new[] { num };
+        } 
+    }
+}
---
+++ PrimeFactorsTest/PrimeFactorTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimeFactorsKata;
+
+namespace PrimeFactorsTest
+{
+    [TestClass]
+    public class PrimeFactorTests
+    {
+        private PrimeFactor primeFactor;
+
+        public PrimeFactorTests()
+        {
+            primeFactor = new PrimeFactor();
+        }
+
+        [TestMethod]
+        public void GenerateWithOneReturnsNull()
+        {
+            Assert.IsNull(primeFactor.Generate(1));
+        }
+
+        [TestMethod]
+        public void GenerateWithTwoReturnsTwo()
+        {
+            CollectionAssert.AreEqual(new[] { 2 }, primeFactor.Generate(2));
+        }
+
+        [TestMethod]
+        public void GenerateWithThreeReturnsThree()
+        {
+            CollectionAssert.AreEqual(new[] { 3 }, primeFactor.Generate(3));
+        }
+    }
+}
